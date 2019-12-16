df47f01246a6bdcfcc4303b9ea650e2c37bbfc61
Mok Ka Chun <mkc188@users.noreply.github.com>
2019-12-16 15:12:24 +0800

    Dirty fix getStringSet()

--- android/src/main/java/com/github/drydart/flutter_android/SharedPreferencesHandler.java
+++ android/src/main/java/com/github/drydart/flutter_android/SharedPreferencesHandler.java
@@ -7,6 +7,12 @@ import android.content.SharedPreferences;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import android.text.TextUtils;
 
 /** SharedPreferencesHandler */
 class SharedPreferencesHandler extends FlutterMethodCallHandler {
@@ -30,7 +36,19 @@ class SharedPreferencesHandler extends FlutterMethodCallHandler {
         final String name = getRequiredArgument(call, "name");
         final int mode = getRequiredArgument(call, "mode");
         final SharedPreferences prefs = context.getSharedPreferences(name, mode);
-        result.success(prefs.getAll());
+        
+        Map<String, ?> allPrefs = prefs.getAll();
+        Map<String, Object> filteredPrefs = new HashMap<>();
+        for (String key : allPrefs.keySet()) {
+          Object value = allPrefs.get(key);
+          if (value instanceof Set) {
+            ArrayList<String> listValue = new ArrayList<>((Set) value);
+            value = TextUtils.join("``", listValue);
+          }
+          filteredPrefs.put(key, value);
+        }
+        
+        result.success(filteredPrefs);
         break;
       }
       default: {
